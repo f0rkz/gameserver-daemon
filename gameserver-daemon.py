47abbe8f57e044618d5ecc9f5858806ead3b2392
f0rkz <f0rkz@work.f0rkznet.net>
2015-05-19 15:02:40 -0400

    First commit

---
+++ gameserver-daemon.py
@@ -0,0 +1,112 @@
+import sys
+import os.path
+import ConfigParser
+import tarfile
+import urllib
+import subprocess
+import screenutils
+
+parser = ConfigParser.SafeConfigParser()
+
+config_file = "server.conf"
+steamcmd_download = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz"
+
+def steamcmd_update(myappid, mysteamcmdpath, mypath, mylogin, mypassword):
+	steamcmd_run = mysteamcmdpath + "steamcmd.sh +login " + mylogin + " " + mypassword + " +force_install_dir " + mypath + " +app_update " + myappid + " validate +quit"
+	return steamcmd_run
+
+if os.path.isfile(config_file):
+	#load configuration objects here
+	parser.read(config_file)
+	# Parse configuration to variables
+	appid = parser.get("gameserver", "appid")
+	path = parser.get("gameserver", "path")
+	gameserver_name = parser.get("gameserver", "gameserver_name")
+	gameserver_ip = parser.get("gameserver", "gameserver_ip")
+	gameserver_port = parser.get("gameserver", "gameserver_port")
+	hostname = parser.get("gameserver", "hostname")
+	gameserver_daemon = parser.get("gameserver", "gameserver_daemon")
+	steam_login = parser.get("steamcmd", "login")
+	steam_password = parser.get("steamcmd", "password")
+
+else:
+	#Prompt for configuration information
+	print "A configuration file was not found. Doing a first run."
+
+	appid = raw_input("Steam AppID: ")
+	if appid == '':
+		sys.exit("No appid given. Please supply an appid.")
+
+	path = raw_input("Gameserver Install Path (with trailing slash): [/home/steam/" + appid + "] ")
+	if path == '':
+		path = "/home/steam/" + appid + "/"
+
+	gameserver_name = raw_input("Gameserver name IE: csgo: [" + appid + "] ")
+	if gameserver_name == '':
+		gameserver_name = appid
+
+	steam_login = raw_input("Steam login: [anonymous] ")
+	if steam_login == '':
+		steam_login = "anonymous"
+
+	steam_password = raw_input("Steam password: [anonymous] ")
+	if steam_password == '':
+		steam_password = "anonymous"
+
+	gameserver_daemon = raw_input("Gameserver Daemon: [srcds_run] ")
+	if gameserver_daemon == '':
+		gameserver_daemon = "srcds_run"
+
+	hostname = raw_input("Gameserver Hostname: [My Gameserver] ")
+	if hostname == '':
+		hostname = "My Gameserver"
+
+	gameserver_ip = raw_input("Gameserver IP: [0.0.0.0] ")
+	if gameserver_ip == '':
+		gameserver_ip = '0.0.0.0'
+
+	gameserver_port = raw_input("Gameserver Port: [27015] ")
+	if gameserver_port == '':
+		gameserver_port = '27015'
+	
+	# Generate the configuration file
+	parser.add_section('gameserver')
+	parser.add_section('steamcmd')
+	parser.set('gameserver', 'appid', appid)
+	parser.set('gameserver', 'path', path)
+	parser.set('gameserver', 'gameserver_name', gameserver_name)
+	parser.set('gameserver', 'gameserver_daemon', gameserver_daemon)
+	parser.set('gameserver', 'hostname', hostname)
+	parser.set('gameserver', 'gameserver_ip', gameserver_ip)
+	parser.set('gameserver', 'gameserver_port', gameserver_port)
+	parser.set('steamcmd', 'login', steam_login)
+	parser.set('steamcmd', 'password', steam_password)
+
+	# Write the configuration file
+	parser.write(open(config_file, 'w'))
+	print "Configuration file saved as " + config_file
+
+# Now that our configuration is out of the way, let's move on to installing and updating gameserver files
+
+# Check if steamcmd is installed, if not, run it.
+if os.path.isfile(path + "steamcmd.sh"):
+	#Steamcmd is installed in the install path
+	print "steamcmd is installed. Updating gameserver files. This may take a while..."
+	subprocess.call(steamcmd_update(appid, path, path + gameserver_name , steam_login, steam_password), shell=True)
+
+else:
+	#Download steamcmd and extract it
+	urllib.urlretrieve(steamcmd_download, path + "steamcmd_linux.tar.gz")
+	steamcmd_tar = tarfile.open(path + "steamcmd_linux.tar.gz", 'r:gz')
+	steamcmd_tar.extractall(path)
+	print "Steamcmd installed. Starting first run. Downloading gamefiles. This may take a while..."
+	subprocess.call(steamcmd_update(appid, path, path + gameserver_name , steam_login, steam_password), shell=True)
+
+print "All done installing/updating gameserver files. Launching the server."
+
+if gameserver_daemon == "srcds_run":
+	# Gameserver is srcds based. This part is easy.
+	exit()
+elif gameserver_daemon == "whatever_killing_floor_is":
+	# Gameserver is killing floor. Need to do up a custom command here.
+	exit()
