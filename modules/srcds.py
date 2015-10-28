b186905b602dc7fb9cb8a0fd66d8d17fe2306a7b
f0rkz <f0rkz@f0rkznet.net>
2015-10-28 13:49:15 -0400

    Every day im fixing it

--- modules/srcds.py
+++ modules/srcds.py
@@ -17,6 +17,29 @@ class SRCDS(GameServer):
         super(GameServer, self).__init__()
         self.gsconfig = gsconfig
         self.steam_appid = self.gsconfig['steamcmd']['appid']
+
+    def configure_list(self, group, options):
+        """
+        Method used to loop through configuration lists and prompt the user
+        """
+        for config_object in options:
+            while True:
+                user_input = raw_input(config_object['info'])
+                if user_input:
+                    if config_object.get('valid_option') and not user_input in config_object['valid_option']:
+                        print "Invalid option. Please chose one of the following: {}".format(config_object['valid_option'])
+                    else:
+                        group[config_object['option']] = user_input
+                        break
+                if not config_object.get('default', None):
+                    #loop back and ask again
+                    pass
+                else:
+                    # Default value set!
+                    group[config_object['option']] = config_object['default']
+                    break
+            parser.set(group['id'], config_object['option'], group[config_object['option']])
+
     def configure(self):
         config_options = [
             {'option': 'hostname', 'info': 'Gameserver hostname: [My Gameserver] ', 'default': 'My Gameserver'},
@@ -55,6 +78,6 @@ class SRCDS(GameServer):
         ]
         myid = {'id': self.steam_appid}
         parser.add_section(self.steam_appid)
-        GameServer.configure_list(myid,config_options)
+        self.configure_list(myid,config_options)
         parser.write(open(CONFIG_FILE, 'w'))
         print "SRCDS configuration saved as {}".format(CONFIG_FILE)
--- modules/unreal.py
+++ modules/unreal.py
@@ -17,6 +17,28 @@ class Unreal(GameServer):
         super(GameServer, self).__init__()
         self.steam_appid = self.gsconfig['steamcmd']['appid']
 
+    def configure_list(self, group, options):
+        """
+        Method used to loop through configuration lists and prompt the user
+        """
+        for config_object in options:
+            while True:
+                user_input = raw_input(config_object['info'])
+                if user_input:
+                    if config_object.get('valid_option') and not user_input in config_object['valid_option']:
+                        print "Invalid option. Please chose one of the following: {}".format(config_object['valid_option'])
+                    else:
+                        group[config_object['option']] = user_input
+                        break
+                if not config_object.get('default', None):
+                    #loop back and ask again
+                    pass
+                else:
+                    # Default value set!
+                    group[config_object['option']] = config_object['default']
+                    break
+            parser.set(group['id'], config_object['option'], group[config_object['option']])
+
     def configure(self):
         config_options = [
             {'option': 'ip', 'info': 'Gameserver IP: [0.0.0.0] ', 'default': '0.0.0.0'},
